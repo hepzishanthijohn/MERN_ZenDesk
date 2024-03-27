6d5566a0b366a9fbc6310139ae2077600bfb9fce
hepzishanthijohn <yyepsipal@gmail.com>
2024-03-27 14:51:42 +0530

    MernStack

--- Back-end/server.js
+++ Back-end/server.js
@@ -19,8 +19,7 @@ app.use('/users',userRoutes);
 app.use('/members',memberRoutes);
 
 const PORT = process.env.PORT || 5000;
-const DATABASEURL =process.env.DB_HOST ||  'mongodb://127.0.0.1:27017';
-connectDB(DATABASEURL)
+
 app.listen(PORT, ()=>{
     console.log(`server running on port : http://localhost:${PORT}/members`)
 })
